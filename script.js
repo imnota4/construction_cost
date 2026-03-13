d348f04acf3cda4f876a0fe94dc0f816677a5354
Kate Hamel <imnota9@gmail.com>
2026-03-13 13:32:43 -0400

    Added error margin

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ let costSqFt = parseFloat(document.getElementById("costSqFt").value)
 let sqFtUnit = parseFloat(document.getElementById("sqFtUnit").value)
 let nonUnit = parseFloat(document.getElementById("nonUnit").value)/100
 let units = parseFloat(document.getElementById("units").value)
+let error = parseFloat(document.getElementById("error").value)/100
 
 let unitSpace = sqFtUnit * units
 
 let totalSqFt = unitSpace / (1 - nonUnit)
 
-let totalCost = totalSqFt * costSqFt
+let baseCost = totalSqFt * costSqFt
+
+let totalCost = baseCost * (1 + error)
 
 document.getElementById("totalSqFt").innerText =
 Math.round(totalSqFt).toLocaleString() + " sq ft"
+
+document.getElementById("baseCost").innerText =
+"$" + Math.round(baseCost).toLocaleString()
 
 document.getElementById("totalCost").innerText =
 "$" + Math.round(totalCost).toLocaleString()
